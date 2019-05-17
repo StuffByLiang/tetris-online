9bf60f4f51a0540842f3cacebf12f63f531bca74
fwang28 <41319051+fwang28@users.noreply.github.com>
2019-05-16 23:52:17 -0700

    Update toggleSettings.js

--- src/client/settings/toggleSettings.js
+++ src/client/settings/toggleSettings.js
@@ -11,5 +11,11 @@ window.toggleSettings = function() {
     $("#gravity").val(settings.gravity);
     $("#softDrop").val(settings.softDrop);
 
+
+  // cookies idk if it works
+  Cookies.set('das', settings.das, { expires: 999999999 });
+  Cookies.set('arr', settings.arr, { expires: 999999999 });
+  Cookies.set('gravity', settings.gravity, { expires: 999999999 });
+  Cookies.set('softDrop', settings.softDrop, { expires: 999999999 });
   }
 }
