9540fe434fce66dc9b57ed81e4fbdce6160f462b
fwang28 <41319051+fwang28@users.noreply.github.com>
2019-05-16 23:55:45 -0700

    Update saveSettings.js

--- src/client/settings/saveSettings.js
+++ src/client/settings/saveSettings.js
@@ -3,8 +3,14 @@ window.saveSettings = function() {
   //remember, to get variables, use $("#das").val()
   settings.das = parseInt(()$("#das").val()), 10);
   settings.arr = parseInt($("#arr").val()), 10);
-  settings.gravity = parsInt($("#gravity").val()),10);
+  settings.gravity = parseInt($("#gravity").val()),10);
   settings.softDrop = parseInt($("#softDrop").val()),10);
+
+  // cookies i think
+  Cookies.set('das', settings.das, { expires: 999999999 });
+  Cookies.set('arr', settings.arr, { expires: 999999999 });
+  Cookies.set('gravity', settings.gravity, { expires: 999999999 });
+  Cookies.set('softDrop', settings.softDrop, { expires: 999999999 });
 
   toggleSettings(); //closes settings box
 }
