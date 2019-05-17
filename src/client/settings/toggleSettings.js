9b8b9a3148f321e4b6b30e08d6b6eb4cd73165c5
Andrew Xie <andrewxieax1@gmail.com>
2019-05-16 23:39:20 -0700

    set initial settings in settings box

--- src/client/settings/toggleSettings.js
+++ src/client/settings/toggleSettings.js
@@ -4,5 +4,12 @@ window.toggleSettings = function() {
     $('#settings-container').hide();
   } else {
     $('#settings-container').show();
+
+    //Makes sure that the values of the shown in text boxes match settins
+    $("#das").val() = settings.das;
+    $("#arr").val() = settings.arr;
+    $("#gravity").val() = settings.gravity;
+    $("#softDrop").val() = settings.softDrop;
+
   }
 }
