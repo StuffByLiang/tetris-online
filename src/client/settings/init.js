cf5ca3e47069daf6993cd8f03c999fcafe203c5c
fwang28 <41319051+fwang28@users.noreply.github.com>
2019-05-17 00:26:45 -0700

    Update init.js

--- src/client/settings/init.js
+++ src/client/settings/init.js
@@ -1,8 +1,8 @@
 window.settings = {
-    arr : 0,
-    das : 120,
-    gravity : 1000,
-    softDrop: 10,
+    arr : Cookies.get('arr') === undefined ? 0 : Cookies.get('arr'),
+    das : Cookies.get('das') === undefined ? 120: Cookies.get('das'),
+    gravity : Cookies.get('gravity') === undefined ? 1000: Cookies.get('gravity'),
+    softDrop: Cookies.get('softDrop') === undefined ? 10: Cookies.get('softDrop'),
     handicap: false,
     myNameIsMeidy: false
 };
